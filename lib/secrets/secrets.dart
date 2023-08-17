6e2e4584e5b85ec3a9b577cf9d533511dbcfa60c
pratimaahet <69415006+pratimaahet@users.noreply.github.com>
2023-08-17 11:42:17 +0530

    Update secrets.dart

--- lib/secrets/secrets.dart
+++ lib/secrets/secrets.dart
@@ -2,9 +2,9 @@ import 'dart:io' show Platform;
 
 class Secret {
   static const ANDROID_CLIENT_ID =
-      "<319846761591-fhba245vbvjqmpee6hhlcu8jl904vp91.apps.googleusercontent.com>";
+      "Your Andriod Client ID here";
   static const IOS_CLIENT_ID =
-      "<319846761591-i5ibe2jq3lug0bll2onmk0tn3ct2oos3.apps.googleusercontent.com>";
+      "Your IOS Client ID Here ";
   static String getId() =>
       Platform.isAndroid ? Secret.ANDROID_CLIENT_ID : Secret.IOS_CLIENT_ID;
 }
